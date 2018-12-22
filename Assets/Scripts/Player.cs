5c24ef390239d7aaff7d19b73c6caaaaf3a80c24
Shawn Choi <shawn.choi@us.imshealth.com>
2018-12-21 23:45:22 -0800

    Add shooting feature to the player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,11 +8,18 @@ public class Player : MonoBehaviour {
     private float moveSpeed = 10f;
     [SerializeField]
     private float padding = 0.6f;
+    [SerializeField]
+    private GameObject lazerPrefab;
+    [SerializeField]
+    private float lazerSpeed = 10f;
+    [SerializeField]
+    private float lazerFiringPeriod = 0.1f;
 
     private float xMin;
     private float xMax;
     private float yMin;
     private float yMax;
+    private Coroutine firingCoroutine;
 
     void Start()
     {
@@ -35,6 +42,29 @@ public class Player : MonoBehaviour {
     void Update()
     {
         this.MovePlayer();
+        this.FireLazer();
+    }
+
+    private void FireLazer()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            this.firingCoroutine = base.StartCoroutine(this.FireContinuously());
+        }
+        else if (Input.GetButtonUp("Fire1"))
+        {
+            base.StopCoroutine(this.firingCoroutine);
+        }
+    }
+
+    private IEnumerator FireContinuously()
+    {
+        while (true)
+        {
+            GameObject lazer = Object.Instantiate(this.lazerPrefab, this.transform.position, Quaternion.identity);
+            lazer.GetComponent<Rigidbody2D>().velocity = new Vector2(0, this.lazerSpeed);
+            yield return new WaitForSeconds(this.lazerFiringPeriod);
+        }
     }
 
     private void MovePlayer()
